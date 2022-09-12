f7376822c1ba89737aed5760af8d799aafeee759
student <shakedgo@gmail.com>
2022-09-12 23:33:25 +0300

    Changed mongo to update with upsert

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const chrono = require("chrono-node");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const { dirxml } = require("console");
+
 const uri = "mongodb+srv://shakedgo:1234567890@cluster0.7rxdu3v.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
@@ -23,63 +23,65 @@ app.get("/api", (_req, res) => {
 });
 
 app.get("/get-paste", (_req, res) => {
-	let author = [];
-	let title = [];
-	let date = [];
-	let text = [];
-	let ids = [];
-	axios({
-		url: "http://paste2vljvhmwq5zy33re2hzu4fisgqsohufgbljqomib2brzx3q4mid.onion/lists",
-		proxy: {
-			host: "localhost",
-			port: 8118,
-		},
-	})
-		.then((page) => {
-			const $ = cheerio.load(page.data);
-			$(".first").each((i, element) => {
-				title[i] = $(element).text().trimStart().trimEnd();
-				$(element)
-					.siblings("td")
-					.each((siblingIndex, sibling) => {
-						if (siblingIndex === 0) author[i] = $(sibling).text();
-						if (siblingIndex === 2) {
-							date[i] = chrono.parseDate($(sibling).text()).toUTCString();
-						}
-					});
-				const id = $(element).find("a").attr("href").split("/")[4];
-				axios({
-					url: `http://paste2vljvhmwq5zy33re2hzu4fisgqsohufgbljqomib2brzx3q4mid.onion/view/raw/${id}`,
-					proxy: {
-						host: "localhost",
-						port: 8118,
-					},
-				})
-					.then((textFetch) => {
-						text[i] = textFetch.data.trimStart().trimEnd();
-						ids[i] = id;
-						let arrobj = [];
-						if (text.length === 50) {
-							for (let i = 0; i < title.length; i++) {
-								arrobj.push({
-									id: ids[i],
-									author: author[i],
-									title: title[i],
-									text: text[i],
-									date: date[i],
-								});
-							}
-							// arrobj.forEach((paste) => {
-							// 	const collection = client.db("DarkPaste").collection("pastes");
-							// 	collection.insertOne(paste);
-							// });
-							res.send(arrobj);
-						}
-					})
-					.catch(console.log("error with paste call"));
+	const fetch = async () => {
+		let author = [];
+		let title = [];
+		let date = [];
+		let text = [];
+		let ids = [];
+		let page = await axios({
+			url: "http://paste2vljvhmwq5zy33re2hzu4fisgqsohufgbljqomib2brzx3q4mid.onion/lists",
+			proxy: {
+				host: "localhost",
+				port: 8118,
+			},
+		});
+
+		const $ = cheerio.load(page.data);
+		$(".first").each(async (i, element) => {
+			title.push($(element).text().trimStart().trimEnd());
+			$(element)
+				.siblings("td")
+				.each((siblingIndex, sibling) => {
+					if (siblingIndex === 0) author.push($(sibling).text());
+					if (siblingIndex === 2) {
+						date.push(chrono.parseDate($(sibling).text()).toUTCString());
+					}
+				});
+			const id = $(element).find("a").attr("href").split("/")[4];
+			let textFetch = await axios({
+				url: `http://paste2vljvhmwq5zy33re2hzu4fisgqsohufgbljqomib2brzx3q4mid.onion/view/raw/${id}`,
+				proxy: {
+					host: "localhost",
+					port: 8118,
+				},
 			});
-		})
-		.catch(console.log("error with website call"));
+			text.push(textFetch.data);
+			ids.push(id);
+
+			let arrobj = [];
+			if (text.length === 50) {
+				for (let i = 0; i < title.length; i++) {
+					const collection = client.db("DarkPaste").collection("pastes");
+					collection.updateOne(
+						{ _id: ids[i] },
+						{
+							$set: {
+								author: author[i],
+								title: title[i],
+								text: text[i],
+								date: date[i],
+							},
+						},
+						{ upsert: true }
+					);
+					arrobj.push({ id: ids[i], author: author[i], title: title[i], text: text[i], date: date[i] });
+				}
+				res.send(arrobj);
+			}
+		});
+	};
+	fetch();
 });
 
 app.get("/get", (_req, res) => {
