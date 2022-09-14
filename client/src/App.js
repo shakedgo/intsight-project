23f72997a83198d7b9be5a3f6116146f257765de
student <shakedgo@gmail.com>
2022-09-14 14:58:39 +0300

    Added search by author/title/text

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ export default function App() {
 	const [pastes, setPastes] = useState();
 	const [searchTerm, setSearchTerm] = useState();
 	const [allPastes, setAllPastes] = useState();
+	const [filter, setFilter] = useState("author");
 
 	useEffect(() => {
 		axios.get("/get-pastes").then((res) => {
@@ -17,8 +18,15 @@ export default function App() {
 	useEffect(() => {
 		const search = () => {
 			let newPaste = [];
+			console.log(filter);
 			allPastes.forEach((res) => {
-				if (res.title.includes(searchTerm)) newPaste.push(res);
+				if (filter === "author") {
+					if (res.author.includes(searchTerm)) newPaste.push(res);
+				} else if (filter === "title") {
+					if (res.title.includes(searchTerm)) newPaste.push(res);
+				} else if (filter === "text") {
+					if (res.text.includes(searchTerm)) newPaste.push(res);
+				}
 			});
 			setPastes(newPaste);
 		};
@@ -26,12 +34,19 @@ export default function App() {
 			if (searchTerm !== undefined) search();
 		}, 300);
 		return () => clearTimeout(timeoutId);
-	}, [searchTerm, allPastes]);
+	}, [searchTerm, allPastes, filter]);
 	return (
 		<>
 			{pastes !== undefined ? (
 				<div className="post">
 					<input key="inpt" type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+					<select onChange={(e) => setFilter(e.target.value)}>
+						<option value="author" selected>
+							Author
+						</option>
+						<option value="title">Title</option>
+						<option value="text">Text</option>
+					</select>
 					{pastes.map((paste) => {
 						return <PasteCard paste={paste} />;
 					})}
